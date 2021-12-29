1842083712fd0959438990300b0cd2c8812d7fc5
Pedro <pedrorsehn@hotmail.com>
2021-12-29 18:16:56 -0300

    add desafio1 lint

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,9 +1,9 @@
 db.movies.aggregate([{
-    $match: {
-      "ibm.rating": { $gte: 7 },
-      genres: { $nin: ["Crime", "Horror"] },
-      rated: { $in: ["PG", "G"]},
-      languages: { $all: ["English", "Spanish"] },
-    },
+  $match: {
+    "ibm.rating": { $gte: 7 },
+    genres: { $nin: ["Crime", "Horror"] },
+    rated: { $in: ["PG", "G"] },
+    languages: { $all: ["English", "Spanish"] },
   },
+},
 ]);
